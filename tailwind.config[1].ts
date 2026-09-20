cb917b982d8ff8d7353c536d01b35beed1990a23
mollieguess <mollieguess@gmail.com>
2026-09-19 19:02:54 -0500

    Add files via upload

---
+++ tailwind.config[1].ts
@@ -0,0 +1,20 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      colors: {
+        tealBrand: {
+          100: '#ccfbf1',
+          500: '#14b8a6',
+          600: '#0d9488',
+          700: '#0f766e',
+          800: '#115e59',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
